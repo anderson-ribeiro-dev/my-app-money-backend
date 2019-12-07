a68ed830d78a20b0d257315b6577a1282ed7f434
anderson silva <anderson_silvaribeiro@hotmail.com>
2019-12-07 20:15:35 -0300

    habilitando cors, chamadas

--- src/config/server.js
+++ src/config/server.js
@@ -3,9 +3,11 @@ const port = 3003
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
+const allowCors = require('./cors')
 
 server.use(bodyParser.urlencoded({ extended: true })) //padrão submit form
 server.use(bodyParser.json())//middleware parse-json
+server.use(allowCors) //middleware de chamadas
 
 server.listen(port, function(){
    console.log(`Backend is running on port ${port}`)     
